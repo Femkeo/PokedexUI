36ce7a28d13ad2ab0869d384be450155aafeb1a7
Femkeo <foffringa@mobiquityinc.com>
2019-07-26 14:43:20 +0200

    Initial Commit

---
+++ PokedexUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PokedexUI
+//
+//  Created by Femke Offringa on 26/07/2019.
+//  Copyright © 2019 Femke Offringa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
