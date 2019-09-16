f9df7589f30273606778221e73ce4f41f9a2629e
Femkeo <foffringa@mobiquityinc.com>
2019-09-16 12:58:01 +0200

    listView try

--- PokedexUI/ContentView.swift
+++ PokedexUI/ContentView.swift
@@ -9,15 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var pokemon: [Pokemon]
+    
     var body: some View {
-        Text("Hello World")
+        ZStack{
+            Color.blue
+            .edgesIgnoringSafeArea(.all)
+            VStack{
+                List(pokemon){ poke in
+                    pokeImageCell(pokemon: poke)
+                    .foregroundColor(Color.gray)
+                    .background(Color.yellow)
+                }.background(Color.red)
+            }.background(Color.clear)
+        }
     }
 }
 
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(pokemon: testData)
     }
 }
 #endif
+
+
+struct pokeImageCell : View{
+    let pokemon : Pokemon?
+    
+    var body: some View {
+        return HStack{
+            Image("Charmander")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .leading)
+            Text(pokemon?.name ?? "")
+        }
+    }
+    
+}
